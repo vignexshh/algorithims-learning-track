26b1162f8e2baa4da7b6dbb0b06cec8e61711442
Vigensh T D <vigneshtd@outlook.com>
2026-03-04 19:51:33 +0530

    comments on variable sw type

--- sliding-window/variable_sliding_window.py
+++ sliding-window/variable_sliding_window.py
@@ -1,4 +1,9 @@
 #goal is to determine longest subarray sum <= k
+
+# use a global variable to track sum
+# add right element
+# if the sum exceeds the k limit, remove left element until it satisfies condtion
+
 def longest_subarray_sum(arr, k):
     window_sum = 0
     max_len = 0
@@ -11,10 +16,9 @@ def longest_subarray_sum(arr, k):
             window_sum -= arr[left]
             left += 1
 
-        max_len = max(max_len, right - left + 1)
+        max_len = max(max_len, right - left + 1) # determines max len of subarray
 
-
-
+    return max_len
 
 
 arr = [1, 2, 3, 4, 5]
